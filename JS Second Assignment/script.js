5eaefc51370b562817e3748020950b4d83564bbb
Bisikha26 <bisikhasubedi26@gmail.com>
2020-07-15 15:57:39 +0545

    Q2 Added

--- JS Second Assignment/script.js
+++ JS Second Assignment/script.js
@@ -1,4 +1,5 @@
-// Convert list of name in array of objects
+//Q1. Convert list of name in array of objects
+// Map function always returns an array, so whatever you return from the map function will be inside an array
 
 var listOfNames = `Neeta sapkota
 neha shiwakoti
@@ -21,11 +22,11 @@ var fellowarr = listOfNames.split('\n');
 var finalarray = fellowarr.map(function(value, index){
     var obj = {};
     var fullname = value.split(" ");
-    obj.id = index + 1;
 
     var Name = fullname[0];
     var Cast = fullname[1];
 
+    obj.id = index + 1;
     obj.firstName = Name[0].toUpperCase().concat(Name.substring(1));
     obj.lastName = Cast[0].toUpperCase().concat(Cast.substring(1));
 
@@ -33,4 +34,25 @@ var finalarray = fellowarr.map(function(value, index){
     return obj;
 });
 
-console.log(finalarray);
+// Q1. Answer
+// console.log(finalarray);
+
+
+// Q2. Given the result of the above problem, create a function which takes a character and prints the count of first names which starts with letter that letter and which donot start with given letter.
+// finalarray vannay array ko value callback function ma gako chha which is object, object ko firstname ko char at 0 compare gareko chha
+// filter lay pani map lay jastai array return garchaa, so return bhako array ko size calculate garna milchha.
+
+function find(char) {
+    var size = finalarray.filter(function(value) {
+        return value.firstName.charAt(0) === char.toUpperCase();
+    });
+
+    var matches = size.length;
+    var mismatch = finalarray.length - matches;
+
+    console.log( matches + " and " + mismatch );
+}
+
+// Q2 answer
+find('s');
+find('a');
